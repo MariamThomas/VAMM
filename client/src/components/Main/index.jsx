9faa899bb1be7901d44cde1006c7b5f72231d59c
MariamThomas <89578907+MariamThomas@users.noreply.github.com>
2023-02-28 07:20:49 +0530

    Update index.jsx

--- client/src/components/Main/index.jsx
+++ client/src/components/Main/index.jsx
@@ -3,13 +3,13 @@ import styles from "./styles.module.css";
 const Main = () => {
 	const handleLogout = () => {
 		localStorage.removeItem("token");
-		window.location.reload();
+		window.location = "/";
 	};
 
 	return (
 		<div className={styles.main_container}>
 			<nav className={styles.navbar}>
-				<h1>fakebook</h1>
+				
 				<button className={styles.white_btn} onClick={handleLogout}>
 					Logout
 				</button>
